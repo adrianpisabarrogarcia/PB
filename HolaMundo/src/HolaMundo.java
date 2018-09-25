cda0d0fff4df51930d9ec0b2bd8a8580a4e7687b
adriantxu <adriantxu@macbook-air-de-adrian.local>
2018-09-25 19:13:46 +0200

    1.0

---
+++ HolaMundo/src/HolaMundo.java
@@ -0,0 +1,25 @@
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		// HolaMundo
+		System.out.println("Hola Mundo!");
+		// Declaración de Variables
+		double m;
+		double n;
+		// Asignación de valor a variables
+		m = 5.5;
+		n = 3.5;
+
+		// Impresión por pantalla
+		System.out.println();
+		System.out.println("La suma de m y n + 2 es igual a...");
+		System.out.println();
+		System.out.println("m=	" + (m + 2.0));
+		System.out.println("m=	" + (n + 2.0));
+
+		System.out.println("¡Hola Guzman!");
+
+	}
+
+}
