bb8cd076dfc0e6660aaa02e262aa668f258960aa
adriantxu <adriantxu@10.111.86.7>
2018-09-25 12:25:31 +0200

    0.1

--- SalarioNeto/src/Main.java
+++ SalarioNeto/src/Main.java
@@ -1,9 +1,33 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+			
+		//Variables
+		int salarioBase, antiguedad, salarioBruto, numeroQuinquenios, numeroAnyos, pagoQuinquenios
+		,pagoAnyos;
+		double salarioNeto, descuentos;
+		
+		//Constantes
+		final int COMPLEMENTO_QUINQUENIO=60,  COMPLEMENTO_ANUAL=6;
+		
+		//Para que pueda leer datos
+		Scanner entrada = new Scanner(System.in);
+		
+		//Entrada de datos
+		System.out.println("Introduce el salario base del trabajador: ");
+		salarioBase = entrada.nextInt();
+		System.out.println("Introduce la anriguedad del trabajador en años: ");
+		antiguedad = entrada.nextInt();
+		
+		//Cálculo de salario bruto
+		
+		
+		//Imprimir por pantalla el salarioNeto
+		System.out.println("El salario Neto del trabajador es: "`+salarioNeto);
 
+		
 	}
 
 }
